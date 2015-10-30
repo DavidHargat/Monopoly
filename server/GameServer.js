ef16c1deabc1e0b5b5ba129937a73e1b8df54c74
ThroughTheVoid <info_master4@yahoo.com>
2015-10-30 05:14:31 -0400

    changes GameServer.sockets to socketTable, added broadcast and forEachSocket functionality

--- server/GameServer.js
+++ server/GameServer.js
@@ -1,29 +1,68 @@
 
+/**
+* Manages socket connections.
+* Contains game/lobby objects.
+*/
 var GameServer = function(){
 	
-	this.sockets = [];
-		
+	this.socketTable = {};
+	
+	/**
+	* Sends a message to each socket.
+	* @param {string} header
+	* @param {Object} data
+	*/
+	this.broadcast = function( header, data ){
+		this.forEachSocket(function( socket ){
+			socket.emit(header, data);
+		});
+	};
+	
+	/**
+	* Iterates through each socket in the socketTable.
+	* @param {function} callback - the function to call for each socket.
+	*/
+	this.forEachSocket = function(callback){
+		for( id in this.socketTable ){
+			callback( this.socketTable[id] );
+		}
+	};
+	
+	/**
+	* Remove a socket from the socket Array.
+	* @param {Socket} socket - the socket to remove.
+	*/
 	this.removeSocket = function( socket ){
-		var i = this.sockets.indexOf(socket);
-		if( i > -1){
-			this.sockets.splice(i,1);
+		if( socketTable[socket.id] ){
+			delete socketTable[socket.id];
 		}else{
 			console.log("ERROR (GameServer::removeSocket) Could not remove socket ", socket);
 		}
 	};	
 	
+	/**
+	* Add a socket to the socket Array.
+	* @param {Socket} socket - the socket to add.
+	*/
 	this.addSocket = function( socket ){
-		this.sockets.push(socket);
+		this.socketTable[socket.id] = socket;
 	};
 	
+	/**
+	* Handle new socket connections.
+	* @param {Socket} socket - the socket that connected.
+	*/
 	this.onConnect = function( socket ){
 		this.addSocket(socket);
 	};
 	
+	/**
+	* Handles socket disconnections.
+	* @param {Socket} socket - the socket that disconnected.
+	*/
 	this.onDisconnect = function( socket ){
 		this.removeSocket(socket);
 	};
-	
 	
 };
 
